78816655b9ceeeb2f795d144476624476f7b7490
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-10 00:24:10 -0300

    add Fastify server setup with CORS support

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+
+const app = Fastify({ logger: true })
+
+const start = async () => {
+    try{
+        await app.listen({ port: 3333 })
+    } catch(err) {
+        
+    }
+}
