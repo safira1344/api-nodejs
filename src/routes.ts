dd7bb9303767178b9ec6d2db2222a94ff2ad4b41
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-23 23:56:13 -0300

    feat: add delete customer route to handle customer deletion requests

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { type FastifyInstance, type FastifyPluginOptions, type FastifyRequest, type FastifyReply } from "fastify"
 import { CreateCustomerController } from "./controllers/CreateCustomerController.js"
 import {ListCustomersController} from './controllers/ListCustomersController.js'
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController.js"
 
 export async function routes(fastify:FastifyInstance, options:FastifyPluginOptions) {
     fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
@@ -14,5 +15,9 @@ export async function routes(fastify:FastifyInstance, options:FastifyPluginOptio
 
     fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply) => {
         return new ListCustomersController().handle(request, reply)
+    })
+
+    fastify.delete("/customer", async(request: FastifyRequest, reply: FastifyReply) => {
+        return new DeleteCustomerController().handle(request, reply)
     })
 }
