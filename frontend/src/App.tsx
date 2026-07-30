7d40ccc5a71631b2126aed41c36ca0a27cc5864e
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-29 21:35:44 -0300

    refactor: update App component structure and styling for improved layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-
 export default function App() {
 
     return (
-        <>
-            <h1>MEU APP REACT</h1>
-        </>
+        <div className='w-full min-h-screen bg-gray-900 flex justify-center px-4'>
+            <main className="my-10 w-full md:max-w-2xl">
+                <h1 className="text-4xl font-medium text-white">Clientes</h1>
+            </main>
+        </div>
     )
 }
