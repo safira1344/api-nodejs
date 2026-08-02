16d6c392635fbaab617505ed609bb242dd03768b
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-08-01 22:08:33 -0300

    feat: add form for user name input in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,13 @@ export default function App() {
         <div className='w-full min-h-screen bg-gray-900 flex justify-center px-4'>
             <main className="my-10 w-full md:max-w-2xl">
                 <h1 className="text-4xl font-medium text-white">Clientes</h1>
+
+                <form>
+                    <label>Nome:</label>
+                    <input 
+                    type="text"
+                    placeholder="Digite seu nome completo." />
+                </form>
             </main>
         </div>
     )
