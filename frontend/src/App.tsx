4de632851150d027bb45cf435f31f4373e2050f7
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-08-05 23:09:50 -0300

    refactor: enhance form structure and styling in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,12 @@ export default function App() {
             <main className="my-10 w-full md:max-w-2xl">
                 <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
-                <form>
-                    <label>Nome:</label>
+                <form className="flex flex-col my-6">
+                    <label className="font-medium text-white">Nome:</label>
                     <input 
                     type="text"
-                    placeholder="Digite seu nome completo." />
+                    placeholder="Digite seu nome completo..."
+                    className="w-full mb-5 p-2"/>
                 </form>
             </main>
         </div>
